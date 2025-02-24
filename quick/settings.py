388ab4e49a9b34e45aafe4623e6e84b6cedb9850
Brayan <ballajo2270@gmail.com>
2025-02-24 00:26:25 -0500

    .

--- quick/settings.py
+++ quick/settings.py
@@ -106,7 +106,7 @@ SWAGGER_SETTINGS = {
 }
  """
 
-""" REST_FRAMEWORK = {
+REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
@@ -116,16 +116,8 @@ SWAGGER_SETTINGS = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-} """
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',  # 🔹 Permite acceso sin autenticación
-    ),
 }
+
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=7),  # Aumenta si es muy corto
